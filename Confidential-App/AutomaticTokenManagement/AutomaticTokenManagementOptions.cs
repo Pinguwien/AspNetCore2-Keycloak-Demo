df43cf0495f4c12906f954b3ad951bb260810de5
Dominik Guhr <dominik.guhr@codecentric.de>
2019-03-08 13:31:32 +0100

    little cleanup p2

--- Confidential-App/AutomaticTokenManagement/AutomaticTokenManagementOptions.cs
+++ Confidential-App/AutomaticTokenManagement/AutomaticTokenManagementOptions.cs
@@ -8,6 +8,6 @@ namespace IdentityModel.AspNetCore
     {
         public string Scheme { get; set; }
         public TimeSpan RefreshBeforeExpiration { get; set; } = TimeSpan.FromMinutes(1);
-        public bool RevokeRefreshTokenOnSignout { get; set; } = false; //keycloak doesn't suppport that yet... d'uh - see https://issues.jboss.org/browse/KEYCLOAK-5325
+        public bool RevokeRefreshTokenOnSignout { get; set; } = false; //keycloak doesn't suppport that yet - see https://issues.jboss.org/browse/KEYCLOAK-5325
     }
 }
